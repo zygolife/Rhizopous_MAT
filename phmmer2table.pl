aebced720ac5e4da91948fdc2a386ba1148281d7
Jason Stajich <jasonstajich.phd@gmail.com>
2018-01-25 23:48:24 -0800

    add seqs extracted

--- phmmer2table.pl
+++ phmmer2table.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
+my $db = "Rhizopus_peps.aa.fasta";
 my $lookup = 'prefix.tsv';
 my $cutoff = 1e-5;
 my %lookup;
@@ -11,6 +12,7 @@ while(<$fh>) {
     my ($p,$name) = split;
     $lookup{$p} = $name;
 }
+close($fh);
 my (%table, %seengenes);
 while(<>) {
     next if /^\#/ || /^\s+$/;
@@ -24,9 +26,9 @@ while(<>) {
 #	warn("skip\n");
 	next
     }
-    if ( $domain =~ /([^\|]+)\|(\S+)/ ) {
-	$domain = $2;
-    }
+    #if ( $domain =~ /([^\|]+)\|(\S+)/ ) {
+    #	$domain = $2;
+    #}
     $seengenes{$domain}++;
     my ($sppref,$genename) = split(/\|/,$gene_name);
     #warn("sppref is $sppref\n");
@@ -34,9 +36,28 @@ while(<>) {
 	$table{$sppref}->{$domain} = sprintf("%s (%s %s)",$genename,$fullevalue,$fullscore);
     }    
 }
+
 print join("\t", qw(TAXON PREFIX), sort keys %seengenes),"\n";
 for my $p ( sort { $lookup{$a} cmp $lookup{$b} } 
 	    keys %table ) {
     print join("\t", $lookup{$p},$p, map { $table{$p}->{$_} || 0 }
 	       sort keys %seengenes),"\n";
+}
+my $outdir = "query_hit_seqs";
+mkdir($outdir);
+for my $gene ( keys %seengenes ) {
+ open($fh => ">$outdir/$gene.fa") || die $!;
+ for my $taxa ( keys %table ) {
+   my $longname = $lookup{$taxa};
+   if ( exists $table{$taxa}->{$gene} ) {
+	my ($hit,$rest) = split(/\s+/,$table{$taxa}->{$gene},2);
+   	open(my $sfh => "esl-sfetch $db '$taxa|$hit' |") || die $!;
+   	while(<$sfh>) {
+  		s/^>(\S+)\|(\S+)/>$2 $longname $rest/;
+		print $fh $_;
+   	}
+   	close($sfh);
+   }
+  }
+  close($fh);
 }
